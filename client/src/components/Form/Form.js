907d58b8788b167110ea2cbd4c8dfd47942c6dce
ryutheblade <65879071+ryutheblade@users.noreply.github.com>
2022-05-01 03:38:05 -0700

    Added posting

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64'
 import useStyles from './styles';
+import { useDispatch } from 'react-redux'
+import { createPost } from '../../actions/posts'
 
 const Form = () => {
     const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const classes = useStyles();
-    const handleSubmit = () => {
+    const dispatch = useDispatch();
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
