443c02c25a0d66a9d025db6a8646295ee6b418c9
ryutheblade <65879071+ryutheblade@users.noreply.github.com>
2022-06-15 23:58:47 -0700

    Bug fix

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,10 +2,11 @@ import * as api from '../api/index.js';
 
 export const getPosts = (page) => async (dispatch) => {
     try {
+        dispatch({ type: 'START_LOADING' });
         const { data } = await api.fetchPosts(page);
 
-
-        dispatch({ type: 'FETCH_ALL', payload: data })
+        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: 'END_LOADING' });
     } catch (error) {
         console.log(error.message);
     }
@@ -13,19 +14,23 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
-        const { data: { data } } = await api.fetchPostsBySearch(searchQuery)
+        dispatch({ type: 'START_LOADING' });
+        const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
 
-        dispatch({ type: 'FETCH_BY_SEARCH', payload: data })
+        dispatch({ type: 'FETCH_BY_SEARCH', payload: data });
+        dispatch({ type: 'END_LOADING' });
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch({ type: 'START_LOADING' });
         const { data } = await api.createPost(post);
 
         dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: 'END_LOADING' });
     } catch (error) {
         console.log(error);
     }
